c2a60d2c20fcf4ec89da080bed17ceaf5dcdca95
sktandon0121 <sktandon0121@gmail.com>
2019-02-16 17:39:18 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+import DataGrid from './DataGrid/DataGrid';
+
+class App extends Component {
+  
+  render() {
+    return (
+      <div className="App">
+        <DataGrid />
+      </div>
+    );
+  }
+}
+
+export default App;
